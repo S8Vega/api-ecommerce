5409a2f0d38d8c72ec30e34b818027f9578450a7
V8Sebastian <bsebastianvg18@gmail.com>
2020-04-28 22:29:04 -0500

    commit inicial

---
+++ src/main/java/com/servitec/ApiAgsApplication.java
@@ -0,0 +1,13 @@
+package com.servitec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/servitec/ApiAgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servitec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
