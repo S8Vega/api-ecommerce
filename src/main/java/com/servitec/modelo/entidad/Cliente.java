ef6aba2feebb715be1a0d0d33786df44c6a90570
V8Sebastian <bsebastianvg18@gmail.com>
2020-05-07 15:04:10 -0500

    creando nueva rama

---
+++ src/main/java/com/servitec/modelo/entidad/Cliente.java
@@ -0,0 +1,15 @@
+package com.servitec.modelo.entidad;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cliente")
+public class Cliente implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/com/servitec/modelo/entidad/Proveedor.java
@@ -0,0 +1,10 @@
+package com.servitec.modelo.entidad;
+
+import java.io.Serializable;
+
+public class Proveedor implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+
+}
