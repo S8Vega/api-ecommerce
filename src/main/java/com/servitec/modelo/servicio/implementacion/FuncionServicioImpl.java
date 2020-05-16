800b5e534d3d7cc5a1a10fcbc8d36cc4ec07e524
V8Sebastian <bsebastianvg18@gmail.com>
2020-05-16 12:23:26 -0500

    controladores revisados

--- src/main/java/com/servitec/modelo/servicio/implementacion/FuncionServicioImpl.java
+++ src/main/java/com/servitec/modelo/servicio/implementacion/FuncionServicioImpl.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.servitec.modelo.dao.interfaz.IFuncionDao;
+import com.servitec.modelo.dao.interfaz.IProductoDao;
 import com.servitec.modelo.entidad.Funcion;
+import com.servitec.modelo.entidad.Producto;
 import com.servitec.modelo.servicio.interfaz.IServicio;
 
 @Service("FuncionServicioImpl")
@@ -15,6 +17,8 @@ public class FuncionServicioImpl implements IServicio<Funcion, Long> {
 
 	@Autowired
 	private IFuncionDao funcionDao;
+	@Autowired
+	private IProductoDao productoDao;
 
 	@Override
 	@Transactional(readOnly = true)
@@ -25,6 +29,16 @@ public class FuncionServicioImpl implements IServicio<Funcion, Long> {
 	@Override
 	@Transactional
 	public void save(Funcion funcion) {
+		if (!funcion.getProducto().isEmpty()) {
+			List<Producto> productos = (List<Producto>) productoDao.findAll();
+			for (Producto pf : funcion.getProducto()) {
+				for (Producto pp : productos) {
+					if (pf.getProducto_pk().equals(pp.getProducto_pk())) {
+						pp.getFuncion().add(funcion);
+					}
+				}
+			}
+		}
 		this.funcionDao.save(funcion);
 	}
 
@@ -37,6 +51,16 @@ public class FuncionServicioImpl implements IServicio<Funcion, Long> {
 	@Override
 	@Transactional
 	public void deleteById(Long id) {
+		Funcion funcion = this.findById(id); // creo una funcion
+		for (Producto p : funcion.getProducto()) { // busco todos los productos de esa funcion
+			for (Funcion f : p.getFuncion()) { // busco las funciones que tenga ese producto
+				if (f.getFuncion_pk().equals(id)) { // si hay un producto que contenga la funcion que quiero eliminar
+					p.getFuncion().remove(f); // la elimino
+				}
+			}
+		}
+		// ahora elimino todos los productos que tenga la funcion que quiero eliminar
+		funcion.getProducto().removeAll(funcion.getProducto());
 		this.funcionDao.deleteById(id);
 	}
 
--- src/main/java/com/servitec/modelo/servicio/implementacion/ProductoServicioImpl.java
+++ src/main/java/com/servitec/modelo/servicio/implementacion/ProductoServicioImpl.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.servitec.modelo.dao.interfaz.IFuncionDao;
 import com.servitec.modelo.dao.interfaz.IProductoDao;
+import com.servitec.modelo.entidad.Funcion;
 import com.servitec.modelo.entidad.Producto;
 import com.servitec.modelo.servicio.interfaz.IServicio;
 
@@ -15,6 +17,8 @@ public class ProductoServicioImpl implements IServicio<Producto, Long> {
 
 	@Autowired
 	private IProductoDao productoDao;
+	@Autowired
+	private IFuncionDao funcionDao;
 
 	@Override
 	@Transactional(readOnly = true)
@@ -25,6 +29,16 @@ public class ProductoServicioImpl implements IServicio<Producto, Long> {
 	@Override
 	@Transactional
 	public void save(Producto producto) {
+		if (!producto.getFuncion().isEmpty()) {
+			List<Funcion> funciones = (List<Funcion>) funcionDao.findAll();
+			for (Funcion fp : producto.getFuncion()) {
+				for (Funcion ff : funciones) {
+					if (ff.getFuncion_pk().equals(fp.getFuncion_pk())) {
+						ff.getProducto().add(producto);
+					}
+				}
+			}
+		}
 		this.productoDao.save(producto);
 	}
 
@@ -37,6 +51,16 @@ public class ProductoServicioImpl implements IServicio<Producto, Long> {
 	@Override
 	@Transactional
 	public void deleteById(Long id) {
+		Producto producto = this.findById(id); // creo un producto
+		for (Funcion f : producto.getFuncion()) {// busco todas las funciones que tenga ese producto
+			for (Producto p : f.getProducto()) { // busco todos los productos de esa funcion
+				if (p.getProducto_pk().equals(id)) {// si hay una funcion que contenga el producto que quiero eliminar
+					f.getProducto().remove(p);// lo elimino
+				}
+			}
+		}
+		// ahora elimino todas las funciones que tenga el producto que quiero eliminar
+		producto.getFuncion().removeAll(producto.getFuncion());
 		this.productoDao.deleteById(id);
 	}
 
